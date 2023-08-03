08b0915e6fa2a53aa325156536954aa3b9853a5d
Fabian Mauz <fmauz@ipb-halle.de>
2023-08-03 21:12:10 +0200

    add bias parameter to model

--- machine-learning/src/main/java/de/fabianmauz/machinelearning/Chapter_3.java
+++ machine-learning/src/main/java/de/fabianmauz/machinelearning/Chapter_3.java
@@ -19,49 +19,58 @@ public class Chapter_3 {
         TextImport importer = new TextImport(dataLocation, xValuesIndices, yValuesIndices, skipLines, valueSeparator);
         SimpleMatrix[] matrices = importer.importData();
 
-        train(matrices[0], matrices[1], 10000, 0.0001f);
+        double[] parameter = train(matrices[0], matrices[1], 20000, 0.001f);
+
+        System.out.println("---------");
+        System.out.println("trained parameters");
+        System.out.println("w -> " + parameter[0]);
+        System.out.println("b -> " + parameter[1]);
     }
 
-    public static float[] train(SimpleMatrix X, SimpleMatrix Y, int maxIterations, float learnRate) {
-        float w = 0;
-        float b = 0;
+    public static double[] train(SimpleMatrix X, SimpleMatrix Y, int maxIterations, float learnRate) {
+        double w = 0;
+        double b = 0;
 
         for (int i = 0; i < maxIterations; i++) {
             double currentLoss = loss(X, Y, w, b);
             System.out.println("Iteration " + i + " --> " + currentLoss);
-            w -= gradient(X, Y, w) * learnRate;
+            double[] gradients = gradient(X, Y, w, b);
+            w -= gradients[0] * learnRate;
+            b -= gradients[1] * learnRate;
         }
-        return new float[]{w, b};
+        return new double[]{w, b};
     }
 
-    public static double loss(SimpleMatrix X, SimpleMatrix Y, double w, float b) {
+    public static double loss(SimpleMatrix X, SimpleMatrix Y, double w, double b) {
         SimpleMatrix Y_roof = predict(X, w, b);
         double sum = Y.minus(Y_roof).elementPower(2).elementSum();
         return sum / Y.numRows();
     }
 
-    public static double gradient(SimpleMatrix X, SimpleMatrix Y, double w) {
-        SimpleMatrix Y_roof = predict(X, w, 0);
+    public static double[] gradient(SimpleMatrix X, SimpleMatrix Y, double w, double b) {
+        SimpleMatrix Y_roof = predict(X, w, b);
         SimpleMatrix diff = Y_roof.minus(Y);
         double wGradient = 2 * average(crossProduct(diff, X));
-        
-        return wGradient;
+        double bGradient = 2 * average(diff);
+
+        return new double[]{wGradient, bGradient};
     }
 
     public static SimpleMatrix predict(SimpleMatrix X, double w, double b) {
         return X.scale(w).plus(b);
     }
-    public static double average(SimpleMatrix M){
-         double sum = M.elementSum();
-         return sum/M.getNumElements();
+
+    public static double average(SimpleMatrix M) {
+        double sum = M.elementSum();
+        return sum / M.getNumElements();
     }
-    
-    public static SimpleMatrix crossProduct(SimpleMatrix M1,SimpleMatrix M2){
-        double[][] result=new double[M1.numRows()][M1.numCols()];
-        for(int i=0;i<M1.numRows();i++){
-            for(int j=0;j<M1.numCols();j++){
-                result[i][j]=M1.get(i,j)*M2.get(i,j);
-            }    
+
+    public static SimpleMatrix crossProduct(SimpleMatrix M1, SimpleMatrix M2) {
+        double[][] result = new double[M1.numRows()][M1.numCols()];
+        for (int i = 0; i < M1.numRows(); i++) {
+            for (int j = 0; j < M1.numCols(); j++) {
+                result[i][j] = M1.get(i, j) * M2.get(i, j);
+            }
         }
         return new SimpleMatrix(result);
     }
