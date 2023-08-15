07b0d9fd0c7ad5ab35862c6d72aee4176193ec6b
fmauz <fmauz@ipb-halle.de>
2023-08-15 14:52:30 +0200

    feat: add bonus task of chapter 6

--- machine-learning/src/main/java/de/fabianmauz/machinelearning/chapter/chapter_6/Chapter_6.java
+++ machine-learning/src/main/java/de/fabianmauz/machinelearning/chapter/chapter_6/Chapter_6.java
@@ -2,6 +2,7 @@ package de.fabianmauz.machinelearning.chapter.chapter_6;
 
 import static de.fabianmauz.machinelearning.chapter.chapter_5.Chapter_5.printAccuracy;
 import static de.fabianmauz.machinelearning.chapter.chapter_5.Chapter_5.train;
+import de.fabianmauz.machinelearning.classifier.Classify;
 import de.fabianmauz.machinelearning.data.TextImport;
 import de.fabianmauz.machinelearning.helper.Matrix;
 import java.io.IOException;
@@ -15,8 +16,6 @@ public class Chapter_6 {
 
     static protected String dataLocation_train_x = "src/main/resources/chapter_6/mnist_data_X_train.csv";
     static protected String dataLocation_train_y = "src/main/resources/chapter_6/mnist_data_Y_train.csv";
-    static protected String dataLocation_test_x = "src/main/resources/chapter_6/mnist_data_X_test.csv";
-    static protected String dataLocation_test_y = "src/main/resources/chapter_6/mnist_data_Y_test.csv";
 
     static protected int[] xValuesIndices = new int[]{0, 1, 2};
     static protected int[] yValuesIndices = new int[]{};
@@ -33,18 +32,34 @@ public class Chapter_6 {
 
     public static void main(String[] args) throws IOException, Exception {
         SimpleMatrix X_train = prepareData(dataLocation_train_x, 0, createXIndexArray(), new int[]{}, true);
-        SimpleMatrix Y_train = prepareData(dataLocation_train_y, 1,new int[]{}, new int[]{0}, true);
-       
+        double[] accuracy = new double[10];
+        for (int digit_to_check = 0; digit_to_check < 10; digit_to_check++) {
+            SimpleMatrix Y_train = prepareData(dataLocation_train_y, 1, new int[]{}, new int[]{0}, true);
+            final int d = digit_to_check;
+            Matrix.apply(Y_train, (x) -> {
+                return x == d ? 1 : 0;
+            });
 
-        Matrix.apply(Y_train, (x) -> {
-            return x == 5 ? 1 : 0;
-        });
-        int i = 0;
-        SimpleMatrix parameter = train(X_train, Y_train, 100, 0.00001f);
+            SimpleMatrix parameter = train(X_train, Y_train, 100, 0.00001f);
 
-        System.out.println("---------");
+            System.out.println("---------");
 
-        printAccuracy(X_train, parameter, Y_train);
+            accuracy[digit_to_check]=calculate(X_train, parameter, Y_train);            
+        }
+        
+        for(int i=0;i<accuracy.length;i++      ){
+            System.out.println("Digit "+i+" -> "+(Math.round(accuracy[i]*10000d)/100d)+"%");
+        }
+
+    }
+    
+     public static double calculate(SimpleMatrix X, SimpleMatrix w, SimpleMatrix Y) {
+        SimpleMatrix Y_classified = new Classify().classify(X, w);
+        double sum = 0;
+        for (int i = 0; i < Y.getNumElements(); i++) {
+            sum += Math.abs(Y.get(i) - Y_classified.get(i));
+        }
+        return (1 - sum / Y.getNumElements());
 
     }
 
