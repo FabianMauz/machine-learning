311adff1d0bce9e65775f4384e6a0707146bfeba
fmauz <fmauz@ipb-halle.de>
2023-08-23 16:52:36 +0200

    add multo weights to classifier

--- machine-learning/src/main/java/de/fabianmauz/machinelearning/classifier/ClassifyOneOfN.java
+++ machine-learning/src/main/java/de/fabianmauz/machinelearning/classifier/ClassifyOneOfN.java
@@ -9,16 +9,16 @@ import org.ejml.simple.SimpleMatrix;
  */
 public class ClassifyOneOfN {
 
-    public SimpleMatrix classify(SimpleMatrix X, SimpleMatrix w) {
-        
+    public SimpleMatrix classify(SimpleMatrix X, SimpleMatrix... w) {
+
         SimpleMatrix values = new Sigmoid().predict(X, w);
         double[][] back = new double[values.numRows()][2];
         for (int i = 0; i < values.numRows(); i++) {
-           
-            for(int j=0;j<values.numCols();j++){
-                if(values.get(i,j)> back[i][1]){
-                    back[i][1]=values.get(i,j);
-                    back[i][0]=j;
+
+            for (int j = 0; j < values.numCols(); j++) {
+                if (values.get(i, j) > back[i][1]) {
+                    back[i][1] = values.get(i, j);
+                    back[i][0] = j;
                 }
             }
             values.set(i, Math.round(values.get(i)));
